e0f6c55c55d9a0bfecb3b3a9c762e4566d4f8b49
Esa Varemo <esa@kuivanto.fi>
2011-09-13 23:23:31 +0300

    Removed debugging-messages

--- src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplusSignListener.java
+++ src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplusSignListener.java
@@ -1,6 +1,5 @@
 package fi.dy.esav.Minecart_speedplus;
 
-import org.bukkit.block.Sign;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.SignChangeEvent;
 
@@ -14,18 +13,10 @@ public class Minecart_speedplusSignListener extends BlockListener {
 	}
 	
 	public void onSignChange (SignChangeEvent e) {
-		e.getPlayer().sendMessage("created sign");
-		e.getPlayer().sendMessage("1st line: " + e.getLine(0));
 		if(e.getLine(0).equalsIgnoreCase("[msp]")){
-			e.getPlayer().sendMessage("got msp sign");
-			
 			if(e.getLine(1).equalsIgnoreCase("fly") || e.getLine(1).equalsIgnoreCase("nofly")){
-				e.getPlayer().sendMessage("it is a fly sign");
 				if(!(e.getPlayer().hasPermission("msp.signs.fly"))) {
-					e.getPlayer().sendMessage("no perms to fly");
 					e.setLine(0, "NO PERMS");
-				} else {
-					e.getPlayer().sendMessage("you can fly");
 				}
 			} else {
 				boolean error = false;
@@ -45,7 +36,6 @@ public class Minecart_speedplusSignListener extends BlockListener {
 					e.setLine(0, "NO PERMS");
 				}
 				
-				e.getPlayer().sendMessage("error: " + error + ", speed: " + speed);
 			}
 			
 			
