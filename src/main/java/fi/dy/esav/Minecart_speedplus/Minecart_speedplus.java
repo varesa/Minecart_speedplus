d7ae5985eda2f3a4f93496a6d2eeb1ecc639d381
steel9 <steel9apps@gmail.com>
2018-07-14 22:34:34 +0200

    Version 2.0.0
    - Changes to settings is now saved
    - Implemented acceleration and deceleration instead of an instant speed change (when changing speed multiplier with signs)
    - Project changed to target Java 1.8
    - Now building with Spigot API (is probably still compatible with Bukkit)
    Minecart_speedplus should be compatible with Spigot 1.12.2

--- src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplus.java
+++ src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplus.java
@@ -5,12 +5,15 @@ import java.util.logging.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Minecart_speedplus extends JavaPlugin {
+	public static boolean verbose = false;
+	
+	FileConfiguration config;
 
 	Logger log = Logger.getLogger("Minecraft");
 	private final Minecart_speedplusVehicleListener VehicleListener = new Minecart_speedplusVehicleListener(this);
@@ -28,13 +31,23 @@ public class Minecart_speedplus extends JavaPlugin {
 		if ( 0 < multiplier & multiplier <= 50) {
 			speedmultiplier = multiplier;
 			return true;
-		}	return false;
+		}
+		return false;
 	}
 	
 	public void onEnable(){ 		
-		log.info(this.getDescription().getName() + " version "
-				+ this.getDescription().getVersion() + " started.");
-		PluginManager pm = this.getServer().getPluginManager();
+		log.info(getDescription().getName() + " version " + getDescription().getVersion() + " started.");
+
+		config = this.getConfig();
+		config.addDefault("defaultSpeedMultiplier", speedmultiplier);
+		config.addDefault("verbose", false);
+		config.options().copyDefaults(true);
+		saveConfig();
+
+		verbose = config.getBoolean("verbose");
+		speedmultiplier = config.getDouble("defaultSpeedMultiplier");
+
+		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(VehicleListener, this);
 		pm.registerEvents(SignListener, this);
 	} 
@@ -47,31 +60,57 @@ public class Minecart_speedplus extends JavaPlugin {
 	double multiplier;
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){	  
-		  if(cmd.getName().equalsIgnoreCase("msp")){ // If the player typed /basic then do the following...
-			  if(sender instanceof Player) {
-				  Player player = (Player) sender;
-				  if (!player.hasPermission("msp.cmd")) {
-					  player.sendMessage("You don't have the right to do that");
-					  return true;
-				  }
-			  }
-			  
-			  try {
-				  multiplier = Double.parseDouble(args[0]);
-			  } catch (Exception e) {
-				  sender.sendMessage(ChatColor.DARK_BLUE + "Minecart_speed+: Multiplier should be a number");
-				  return false;
-			  }
-			  
-			  result = setSpeedMultiplier(multiplier);
-			  if (result) {
-				  sender.sendMessage(ChatColor.DARK_BLUE + "Minecart_speed+: Speed multiplier for new carts set to: " + multiplier);
-				  return true;
-			  } else {
-				  sender.sendMessage(ChatColor.DARK_BLUE + "minecart_speed+: Value must be non-zero and under 50");
-				  return true;
-			  }			  
-		  } //If this has happened the function will break and return true. if this hasn't happened the a value of false will be returned.
-		  return false;
+		  if (cmd.getName().equalsIgnoreCase("msp")) {
+			if ((sender instanceof Player)) {
+				Player player = (Player) sender;
+				if (!player.hasPermission("msp.cmd")) {
+					player.sendMessage("You don't have the right to do that");
+					return true;
+				}
+			}
+			
+			if (args.length == 0) {
+				sender.sendMessage(ChatColor.DARK_BLUE + "Usage: msp set <multiplier> | msp verbose [true/false]");
+				return true;
+			}
+			switch (args[0]) {
+				case "set":
+					try {
+						multiplier = Double.parseDouble(args[1]);
+					} catch (Exception e) {
+						sender.sendMessage(ChatColor.DARK_BLUE + "Minecart_speed+: ERROR: Multiplier should be a number");
+						return false;
+					}
+	
+					result = setSpeedMultiplier(multiplier);
+					if (result) {
+						sender.sendMessage(ChatColor.DARK_BLUE + "Minecart_speed+: Speed multiplier for new carts set to: "
+								+ multiplier);
+						config.set("defaultSpeedMultiplier", multiplier);
+						saveConfig();
+						return true;
+					}
+					sender.sendMessage(ChatColor.DARK_BLUE + "Minecart_speed+: ERROR: Value must be non-zero and under 50");
+					return true;
+				
+				case "verbose":
+					boolean verbose;
+					if (args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("false")) {
+						verbose = Boolean.valueOf(args[1]);
+					}
+					else {
+						sender.sendMessage(ChatColor.DARK_BLUE + "Minecart_speed+: ERROR: Verbose boolean should either be 'true' or 'false'");
+						break;
+					}
+					
+					Minecart_speedplus.verbose = verbose;
+					config.set("verbose", verbose);
+					saveConfig();
+					return true;
+			}
+
+		} //If this has happened the function will break and return true. if this hasn't happened the a value of false will be returned.
+
+		return false;
 	  }
 }
--- src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplusVehicleListener.java
+++ src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplusVehicleListener.java
@@ -2,6 +2,7 @@ package fi.dy.esav.Minecart_speedplus;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
@@ -31,12 +32,22 @@ public class Minecart_speedplusVehicleListener implements Listener {
 	Logger log = Logger.getLogger("Minecraft");
 
 	boolean error;
-	
-	Vector flyingmod = new Vector(10 , 0.01 , 10);
+
+	Vector flyingmod = new Vector(10, 0.01, 10);
 	Vector noflyingmod = new Vector(1, 1, 1);
 
 	public Minecart_speedplusVehicleListener(Minecart_speedplus instance) {
 		plugin = instance;
+	}
+
+	public static double round(double value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
+
+		long factor = (long) Math.pow(10, places);
+		value = value * factor;
+		long tmp = Math.round(value);
+		return (double) tmp / factor;
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL)
@@ -48,7 +59,7 @@ public class Minecart_speedplusVehicleListener implements Listener {
 
 		}
 	}
-	
+
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onVehicleMove(VehicleMoveEvent event) {
 
@@ -65,13 +76,10 @@ public class Minecart_speedplusVehicleListener implements Listener {
 						blockx = cartx + xmod;
 						blocky = carty + ymod;
 						blockz = cartz + zmod;
-						block = cart.getWorld().getBlockAt(blockx, blocky,
-								blockz);
-						blockid = cart.getWorld().getBlockTypeIdAt(blockx,
-								blocky, blockz);
+						block = cart.getWorld().getBlockAt(blockx, blocky, blockz);
+						blockid = cart.getWorld().getBlockTypeIdAt(blockx, blocky, blockz);
 
-						if (blockid == Material.WALL_SIGN.getId()
-						    || blockid == Material.SIGN_POST.getId()) {
+						if (blockid == Material.WALL_SIGN.getId() || blockid == Material.SIGN_POST.getId()) {
 							Sign sign = (Sign) block.getState();
 							String[] text = sign.getLines();
 
@@ -79,11 +87,11 @@ public class Minecart_speedplusVehicleListener implements Listener {
 
 								if (text[1].equalsIgnoreCase("fly")) {
 									cart.setFlyingVelocityMod(flyingmod);
-									
+
 								} else if (text[1].equalsIgnoreCase("nofly")) {
-									
+
 									cart.setFlyingVelocityMod(noflyingmod);
-									
+
 								} else {
 
 									error = false;
@@ -102,11 +110,71 @@ public class Minecart_speedplusVehicleListener implements Listener {
 									if (!error) {
 
 										if (0 < line1 & line1 <= 50) {
+											// Acceleration/deceleration of cart
+											// speed
+											// Running as separate threads to
+											// not freeze whole plugin while
+											// using Thread.sleep
+											Thread thread = new Thread() {
+												public void run() {
+													double signMultiplier = Double.parseDouble(text[1]);
+													double speedMultiplier = cart.getMaxSpeed() / 0.4D;
 
-											cart.setMaxSpeed(0.4D * Double.parseDouble(text[1]));
+													if (signMultiplier > cart.getMaxSpeed() / 0.4D) {
+														// accelerate
+														double diff = signMultiplier
+																- round(cart.getMaxSpeed() / 0.4D, 1);
+														for (double i = 0; i < diff; i += 0.1) {
+															speedMultiplier += 0.1;
+															cart.setMaxSpeed(0.4D * speedMultiplier);
+															if (Minecart_speedplus.verbose)
+																Bukkit.broadcastMessage(
+																		"Minecart_speed+: Accelerating. Current multiplier: "
+																				+ speedMultiplier);
+															try {
+																Thread.sleep(500);
+															} catch (InterruptedException e) {
+																cart.setMaxSpeed(0.4D * signMultiplier);
+																break;
+															}
+														}
+													} else if (signMultiplier < cart.getMaxSpeed() / 0.4D) {
+														// decelerate
+														double diff = round(cart.getMaxSpeed() / 0.4D, 1)
+																- signMultiplier;
+														for (double i = 0; i < diff; i += 0.1) {
+															speedMultiplier -= 0.1;
+															cart.setMaxSpeed(0.4D * speedMultiplier);
+															if (Minecart_speedplus.verbose)
+																Bukkit.broadcastMessage(
+																		"Minecart_speed+: Decelerating. Current multiplier: "
+																				+ speedMultiplier);
+															try {
+																Thread.sleep(500);
+															} catch (InterruptedException e) {
+																cart.setMaxSpeed(0.4D * signMultiplier);
+																break;
+															}
+														}
+													}
+													// Set the final speed
+													// (multiplier according to
+													// sign) to correct eventual
+													// errors made by
+													// acceleration function
+													cart.setMaxSpeed(0.4D * Double.parseDouble(text[1]));
+													if (Minecart_speedplus.verbose)
+														Bukkit.broadcastMessage(
+																"Final multiplier: " + Double.parseDouble(text[1]));
+												}
+											};
+											thread.start();
+
+											// cart.setMaxSpeed(0.4D *
+											// Double.parseDouble(text[1]));
 
 										} else {
-											
+
 											sign.setLine(2, "  ERROR");
 											sign.setLine(3, "WRONG VALUE");
 											sign.update();
